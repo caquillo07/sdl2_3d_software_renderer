22ca5a9669710baf8f518624a783f9a4af216bf8
Hector Mejia <caquillo722@gmail.com>
2023-12-22 02:49:39 -0500

    formatting

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 
 #include <SDL2/SDL.h>
 
-bool is_running = false;
-SDL_Window* window = NULL;
+bool isRunning         = false;
+SDL_Window* window     = NULL;
 SDL_Renderer* renderer = NULL;
 
-bool initialize_window(void) {
+bool initializeWindow(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Error initializing SDL.\n");
         return false;
@@ -17,9 +17,9 @@ bool initialize_window(void) {
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_BORDERLESS
+        1280,
+        720,
+        SDL_WINDOW_RESIZABLE
     );
     if (!window) {
         fprintf(stderr, "Error creating SDL window.\n");
@@ -40,18 +40,18 @@ void setup(void) {
     // TODO:
 }
 
-void process_input(void) {
+void processInput(void) {
     SDL_Event event;
     SDL_PollEvent(&event);
 
     switch (event.type) {
         case SDL_QUIT:
-            is_running = false;
-        break;
+            isRunning = false;
+            break;
         case SDL_KEYDOWN:
             if (event.key.keysym.sym == SDLK_ESCAPE)
-                is_running = false;
-        break;
+                isRunning = false;
+            break;
     }
 }
 
@@ -69,12 +69,13 @@ void render(void) {
 }
 
 int main(void) {
-    is_running = initialize_window();
+    isRunning = initializeWindow();
 
     setup();
 
-    while (is_running) {
-        process_input();
+    // ReSharper disable once CppDFALoopConditionNotUpdated
+    while (isRunning) {
+        processInput();
         update();
         render();
     }
